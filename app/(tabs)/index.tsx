8862cea170ab9f3af3cd54c633e7bce673fb7b86
Ahmed Khalif Adan <hiddigmoha99@gmail.com>
2025-12-01 12:18:50 +0300

    basic screen related build

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,31 @@
+
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container} >
+      <Text style={styles.content}>im Eng khalif</Text>
+      
+
+     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20,
+  },
+
+
+  content: {
+    fontSize: 24,
+  },
+
+
+
+
+});
---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Settingsscreen = () => {
+  return (
+    <View>
+      <Text>settings</Text>
+    </View>
+  )
+}
+
+export default Settingsscreen
